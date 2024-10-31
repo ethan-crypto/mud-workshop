d8ac1d1cc1362238992a342e6846f7e8f426e5fb
Kevin Ingersoll <kingersoll@gmail.com>
2024-10-31 18:29:49 +0000

    remove rainbowkit dep

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,15 +1,8 @@
-import {
-  connectorsForWallets,
-  getDefaultWallets,
-  WalletList,
-} from "@rainbow-me/rainbowkit";
 import { Chain, http, webSocket } from "viem";
 import { anvil } from "viem/chains";
-import { createConfig } from "wagmi";
-import { passkeyWallet, wiresaw } from "@latticexyz/entrykit/internal";
+import { createWagmiConfig, wiresaw } from "@latticexyz/entrykit/internal";
 import { entryKitConfig } from "./entryKitConfig";
 import { rhodolite, garnet, redstone } from "@latticexyz/common/chains";
-import { chainId } from "./common";
 
 const chains = [
   redstone,
@@ -35,37 +28,21 @@ const chains = [
   } as const satisfies Chain,
 ] as const;
 
-const { wallets: defaultWallets } = getDefaultWallets();
-const wallets: WalletList = [
-  {
-    groupName: "Recommended",
-    wallets: [
-      passkeyWallet({
-        // TODO: allow any chain ID
-        chainId,
-        bundlerTransport: entryKitConfig.bundlerTransport,
-        paymasterAddress: entryKitConfig.paymasterAddress,
-        explorerUrl: entryKitConfig.explorerUrl,
-      }),
-    ],
+const transports = {
+  [anvil.id]: webSocket(),
+  [garnet.id]: http(),
+  [rhodolite.id]: wiresaw(http()),
+  [redstone.id]: http(),
+} as const;
+
+export const wagmiConfig = createWagmiConfig({
+  ...entryKitConfig,
+  appInfo: {
+    ...entryKitConfig.appInfo,
+    name: entryKitConfig.appInfo?.name ?? document.title,
   },
-  ...defaultWallets,
-];
-
-const connectors = connectorsForWallets(wallets, {
-  appName: entryKitConfig.appInfo?.name ?? document.title,
-  projectId: entryKitConfig.walletConnectProjectId,
-});
-
-export const wagmiConfig = createConfig({
-  connectors,
   chains,
-  transports: {
-    [anvil.id]: webSocket(),
-    [garnet.id]: http(),
-    [rhodolite.id]: wiresaw(http()),
-    [redstone.id]: http(),
-  },
+  transports,
   pollingInterval: {
     [anvil.id]: 100,
     [garnet.id]: 2000,
